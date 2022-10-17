bb5acf96912ee7cfedc36756897e042857a7ec40
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-10-17 20:15:49 -0300

    pegando a lista de professores do backend e populando na tela

--- src/hooks/pages/useindex.ts
+++ src/hooks/pages/useindex.ts
@@ -1,37 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Professor } from "../../@types/professor";
+import { ApiService } from "../../services/ApiService";
 
 export function useIndex() {
-    const [listaProfessores, setListaProfessores] = useState<Professor[]>([
-        {
-            id: 1,
-            nome: "Professor 1",
-            foto: "https://github.com/EduardoBran.png",
-            descricao: "Descrição do professor 1",
-            valor_hora: 100
-        },
-        {
-            id: 2,
-            nome: "Professor 2",
-            foto: "https://github.com/EduardoBran.png",
-            descricao: "Descrição do professor 2",
-            valor_hora: 100
-        },
-        {
-            id: 3,
-            nome: "Professor 3",
-            foto: "https://github.com/EduardoBran.png",
-            descricao: "Descrição do professor 3",
-            valor_hora: 100
-        },
-        {
-            id: 4,
-            nome: "Professor 4",
-            foto: "https://github.com/EduardoBran.png",
-            descricao: "Descrição do professor 4",
-            valor_hora: 100
-        },
-    ]);
+    const [listaProfessores, setListaProfessores] = useState<Professor[]>([]);
+
+    useEffect(() => {
+        ApiService.get('/professores').then((resposta) => {
+            setListaProfessores(resposta.data)
+        })
+    }, []);
 
     return {
         listaProfessores: listaProfessores
